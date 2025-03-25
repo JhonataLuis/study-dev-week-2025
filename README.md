cf02cee3a5f567c9f3b47d949323d5cc9c465dc4
Jhonata <jhonatafontesluis23@gmail.com>
2025-03-25 16:07:18 -0300

    Projeto criado
    Signed-off-by: Jhonata <jhonatafontesluis23@gmail.com>

---
+++ README.md
@@ -0,0 +1,1 @@
+# study-dev-week-2025 
---
+++ src/test/java/com/study/StudyDevWeek2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyDevWeek2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
