4c5039ddf9771d61681dcad460c1f9f90f774201
Jhonata <jhonatafontesluis23@gmail.com>
2025-03-25 20:10:00 -0300

    Implementação da camada de dominio
    Signed-off-by: Jhonata <jhonatafontesluis23@gmail.com>

---
+++ src/main/java/com/study/domain/model/Feature.java
@@ -0,0 +1,11 @@
+package com.study.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_feature")
+public class Feature extends BaseItem{
+
+        
+}
