2f50b7d6ccf085189f535173d3235c282c17d89b
Jhonata <jhonatafontesluis23@gmail.com>
2025-03-26 11:39:23 -0300

    Adicionado dependencia openApi para testar a API Rest com uma interface
    Signed-off-by: Jhonata <jhonatafontesluis23@gmail.com>

--- src/main/java/com/study/controller/UserController.java
+++ src/main/java/com/study/controller/UserController.java
@@ -1,8 +1,43 @@
 package com.study.controller;
 
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.study.domain.model.User;
+import com.study.service.UserService;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService uService;
+
+    public UserController(UserService uService){
+        this.uService = uService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        var user = uService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User userToCreate){
+        var userCreated = uService.create(userToCreate);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()//SERVE PARA INFORMAR A LOCALIZAÇÃO DO USUARIO CRIADO
+            .path("/{id}")
+            .buildAndExpand(userCreated.getId())
+            .toUri();
+            return ResponseEntity.created(location).body(userToCreate);
+    }
 
 }
